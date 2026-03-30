fd4c902fec728f68dfac9b6f72e83d5eb86f9dcc
Animacypher <sj2404629@gmail.com>
2026-03-30 09:57:01 +0800

    Day 2

---
+++ 4. Arithmetic Operations/main.c/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // arithmaetic operators
+    // = + - * / % ++ --
+
+    int x = 10;
+    int y = 3;
+    int z = 0;
+
+    // z = x + y;
+    // z = x - y;
+    // z = x * y;
+    // z = x / y;
+    // z = x % y;
+    // x++;
+    // x--;
+
+    // augmented assingment operators
+    // x += 2;
+    // x -= 2;
+    // x *= 2;
+    // x /= 2;
+
+    printf("%d", x);
+
+    return 0;
+}
